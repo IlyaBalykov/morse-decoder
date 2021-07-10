0b90c058d7e095f8c173aca9d54d618ddb3f08d2
IlyaBalykov <balykov.ilya.88@gmail.com>
2021-07-10 20:54:27 +0300

    feat: Add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let num_arr = [];
+    let morse_arr = [];
+    let morse_str = "";
+    let decoded_letter = [];
+    for (let arr = Array.from(expr); arr.length; ) {
+        let spl = arr.splice(0,10);
+        let new_item = spl.join('');
+        num_arr.push(new_item);
+    }
+    for (let i = 0; i < num_arr.length; i++) {
+        for (let arr_from_str = Array.from(num_arr[i]); arr_from_str.length; ) {
+            let spl_two = arr_from_str.splice(0, 2);
+            let new_item_two = spl_two.join('');
+
+            if (new_item_two === "10") {
+                new_item_two = ".";
+                morse_str = morse_str.concat(new_item_two);
+            }
+            else if (new_item_two === "11") {
+                new_item_two = "-";
+                morse_str = morse_str.concat(new_item_two);
+            }
+            else if (new_item_two === "00") {
+
+            }
+        }
+        morse_arr.push(morse_str);
+        morse_str = "";
+    }
+    for (let i = 0; i < morse_arr.length; i++) {
+        let code = morse_arr[i];
+        if (code === "") {
+            decoded_letter.push(" ");
+        } else {
+            for (let key in MORSE_TABLE) {
+                if(key === code) {
+                    decoded_letter.push(MORSE_TABLE[key]);
+                }
+            }
+        }
+    }
+    let decoded_word = decoded_letter.join("");
+    console.log(decoded_word);
+    return decoded_word
 }
 
 module.exports = {
